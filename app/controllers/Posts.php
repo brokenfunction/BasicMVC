a399204c9b6aebcb194ddc89afb38fffa136df8e
rzviazok <romahazvz@gmail.com>
2022-08-13 14:41:01 +0300

    Added Inserting Posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -19,10 +19,39 @@ class Posts extends Controller {
     }
 
     public function add() {
-        $data = [
-            'title' => '',
-            'body' => ''
-        ];
-        $this->view('posts/add', $data);
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_error' => '',
+                'body_error' => ''
+            ];
+            if (empty($data['title'])) {
+                $data['title_error'] = 'Please enter title';
+            }
+            if (empty($data['body'])) {
+                $data['body_error'] = 'Please enter body';
+            }
+
+            if(empty($data['title_error']) && empty($data['body_error'])) {
+                if ($this->postModel->addPost($data)) {
+                    flash('post_message', 'Post Added');
+                    redirect('posts');
+                } else {
+                    die('Something went wrong');
+                }
+            } else {
+                $this->view('posts/add', $data);
+            }
+
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+            $this->view('posts/add', $data);
+        }
     }
 }
